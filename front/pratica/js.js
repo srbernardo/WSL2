afd20896468603177829f2a57fa95396f9168ed1
bernardo <bel_brotelle@hotmail.com>
2021-11-28 09:44:52 -0300

    ok

---
+++ front/pratica/js.js
@@ -0,0 +1,25 @@
+// let s = document.getElementById("btn").value
+
+function digitar(valor){
+    document.calc.display.value += valor
+}
+
+function soma(){
+    let valor1 = document.calc.display.value
+}
+
+function sub(){
+    
+}
+
+function div(){
+    
+}
+
+function mult(){
+    
+}
+
+function apagar(){
+    document.calc.display.value = "" 
+}

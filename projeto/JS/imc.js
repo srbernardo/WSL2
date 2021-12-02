470a6380471b36c6cbeabbec0b64af66449dfaa2
bernardo <bel_brotelle@hotmail.com>
2021-12-02 17:29:29 -0300

    update

--- projeto/JS/imc.js
+++ projeto/JS/imc.js
@@ -6,7 +6,7 @@ function imc() {
     let altura = document.getElementById("altura").value
     let peso = document.getElementById("peso").value
     let media = (peso/(altura*altura))
-    result.textContent = nome + " voce esta com um indece de massa de " + media
+    result.textContent = nome + " voce esta com um indece de massa de " + media.toFixed(2).replace(".",",")
     
 }
 submit.addEventListener("click",imc)

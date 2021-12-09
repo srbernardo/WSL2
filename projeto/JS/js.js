17d899fe39b10d23d3e19f8ae8e362dcf07072fc
bernardo <bel_brotelle@hotmail.com>
2021-12-09 08:34:50 -0300

    style of calc

--- projeto/JS/js.js
+++ projeto/JS/js.js
@@ -1,25 +1,13 @@
 // let s = document.getElementById("btn").value
 
-function digitar(valor){
+function digitar(valor) {
     document.calc.display.value += valor    
 }
 
-function soma(){
-    let valor1 = document.calc.display.value
+function result() {
+    document.calc.display.value = eval(document.calc.display.value)
 }
 
-function sub(){
-    
-}
-
-function div(){
-    
-}
-
-function mult(){
-    
-}
-
-function apagar(){
+function apagar() {
     document.calc.display.value = "" 
 }

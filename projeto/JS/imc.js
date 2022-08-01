b4dd74a02849621963a4b6c49b640a8c70644541
bernardo <bel_brotelle@hotmail.com>
2022-08-01 17:19:08 -0300

    layout para mobile da calc de imc

--- projeto/JS/imc.js
+++ projeto/JS/imc.js
@@ -6,7 +6,27 @@ function imc() {
     let altura = document.getElementById("altura").value
     let peso = document.getElementById("peso").value
     let media = (peso/(altura*altura))
-    result.textContent = nome + " voce esta com um indece de massa de " + media.toFixed(2).replace(".",",")
+    let mess = ""
+
+
+    if(media < 18.5) {
+        mess = "a baixo do peso! "
+    } else if (media >= 18.5 && media < 24.9) {
+        mess = "no peso normal! "
+    } else if (media >= 24.9 && media < 29.9) {
+        mess = "com sobrepeso! "
+    } else if (media >= 29.9 && media < 34.9) {
+        mess = "com obesidade grau 1! "
+    } else if (media >= 34.9 && media < 39.9) {
+        mess = "com obesidade grau 2! "
+    } else {
+        mess = "com obesidade grau 3 ou mórbida! "
+    }
+    result.textContent = nome + " você está " + mess + "Seu IMC é de " + media.toFixed(2).replace(".",",")
+
     
 }
+let concatenar = () => {
+
+} 
 submit.addEventListener("click",imc)
